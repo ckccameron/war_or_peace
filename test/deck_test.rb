dd9246cc630c8b32d8f8f8fc6de5585b43948b7f
Cam C <ckc.cameron@gmail.com>
2020-05-09 15:54:10 -0600

    Passed rank_of_card_at test for deck test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -30,4 +30,18 @@ class DeckTest < Minitest::Test
 
     assert_equal [card1, card2, card3], deck.cards
   end
+
+  def test_it_can_return_rank_of_card
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+
+      deck.rank_of_card_at(0)
+      deck.rank_of_card_at(2)
+
+      assert_equal 12, deck.rank_of_card_at(0)
+      assert_equal 14, deck.rank_of_card_at(2)
+    end
 end
