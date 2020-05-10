e5ab6beb6d9324937032ae2ee0850dba53f4d028
Cam C <ckc.cameron@gmail.com>
2020-05-10 12:05:09 -0600

    Completed Player class

---
+++ test/player_test.rb
@@ -0,0 +1,45 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+
+class PlayerTest < Minitest::Test
+  def test_it_exists
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = [card1,card2,card3]
+    player = Player.new('Clarisa', deck)
+
+
+
+    assert_instance_of Player, player
+  end
+
+  def test_player_has_name_and_deck
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      deck = [card1,card2,card3]
+      player = Player.new('Clarisa', deck)
+
+      assert_equal 'Clarisa', player.name
+      assert_equal deck, player.deck
+  end
+
+  def test_if_player_has_lost
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1,card2,card3])
+    player = Player.new('Clarisa', deck)
+
+    assert_equal false, player.has_lost?
+    player.deck.remove_card
+    player.deck.remove_card
+    player.deck.remove_card
+    assert_equal true, player.has_lost?
+    assert_equal deck, player.deck
+  end
+end
