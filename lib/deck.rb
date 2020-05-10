e84e467e6a28788c5af4050e1e4dccd5c3c990db
Cam C <ckc.cameron@gmail.com>
2020-05-10 11:33:59 -0600

    Completed Iteration 1

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,4 +1,5 @@
 require './lib/card'
+require 'pry'
 
 class Deck
   attr_reader :cards
@@ -16,4 +17,19 @@ class Deck
       card.rank >=11
     end
   end
+
+  def percent_high_ranking
+    high_cards = @cards.select do |card|
+      card.rank >=11
+    end
+    ((high_cards.count.to_f / @cards.count) * 100).round(2)
+  end
+
+  def remove_card
+    @cards.shift
+  end
+
+  def add_card(card)
+    @cards << card
+  end 
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -54,4 +54,26 @@ class DeckTest < Minitest::Test
 
       assert_equal [card1, card3], deck.high_ranking_cards
     end
+
+    def test_it_can_return_percentage_of_high_ranking_cards
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+
+      assert_equal 66.67, deck.percent_high_ranking
+
+      deck.remove_card
+      assert_equal [card2, card3], deck.cards
+      assert_equal [card3], deck.high_ranking_cards
+      assert_equal 50, deck.percent_high_ranking
+
+      card4 = Card.new(:club, '5', 5)
+      deck.add_card(card4)
+
+      assert_equal [card2, card3, card4], deck.cards
+      assert_equal [card3], deck.high_ranking_cards
+      assert_equal 33.33, deck.percent_high_ranking
+    end  
 end
